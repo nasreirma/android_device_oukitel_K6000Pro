3f84114f4d0e1895038f29a802efe6d9bb9e92e8
Ottavio Miele <RacerOttavio97@gmail.com>
2017-05-11 15:00:24 +0200

    Update telecomm.mk

--- product/telecomm.mk
+++ product/telecomm.mk
@@ -2,6 +2,7 @@
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/apns-conf.xml:system/etc/apns-conf.xml \
     $(LOCAL_PATH)/configs/ecc_list.xml:system/etc/ecc_list.xml \
+    $(LOCAL_PATH)/configs/custom.conf:system/etc/custom.conf \
     $(LOCAL_PATH)/configs/spn-conf.xml:system/etc/spn-conf.xml
 
 PRODUCT_PACKAGES += \

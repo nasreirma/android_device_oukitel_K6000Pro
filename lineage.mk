3f0700fde656d309d764ba2b0e6d9a3997406f30
Ottavio Miele <RacerOttavio97@gmail.com>
2017-05-11 02:49:57 +0200

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -18,17 +18,17 @@
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, $(LOCAL_PATH)/device_A7010a48.mk)
+$(call inherit-product, $(LOCAL_PATH)/device_K6000Pro.mk)
 
 # Device display
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # Device identifier
-PRODUCT_BRAND := lenovo
-PRODUCT_DEVICE := A7010a48
-PRODUCT_MANUFACTURER := LENOVO
-PRODUCT_MODEL := Lenovo K4 Note
-PRODUCT_NAME := lineage_A7010a48
-PRODUCT_RELEASE_NAME := A7010a48
+PRODUCT_BRAND := oukitel
+PRODUCT_DEVICE := K6000Pro
+PRODUCT_MANUFACTURER := OUKITEL
+PRODUCT_MODEL := Oukitel K6000 Pro
+PRODUCT_NAME := lineage_K6000Pro
+PRODUCT_RELEASE_NAME := K6000Pro
 PRODUCT_RESTRICT_VENDOR_FILES := false

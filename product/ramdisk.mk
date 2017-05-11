10b505dfd5a3a568ce852085f867acc00a8d3110
Ottavio Miele <Ottavio Miele>
2017-05-11 16:06:28 +0200

    K6000Pro boot.img

--- product/ramdisk.mk
+++ product/ramdisk.mk
@@ -3,13 +3,28 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/factory_init.project.rc:root/factory_init.project.rc \
     $(LOCAL_PATH)/rootdir/factory_init.rc:root/factory_init.rc \
     $(LOCAL_PATH)/rootdir/fstab.mt6735:root/fstab.mt6735 \
-    $(LOCAL_PATH)/rootdir/init.modem.rc:root/init.modem.rc \
+	$(LOCAL_PATH)/rootdir/fstab.swap:root/fstab.swap \
+	$(LOCAL_PATH)/rootdir/init:root/init \
+	$(LOCAL_PATH)/rootdir/init.aee.rc:root/init.aee.rc \
+	$(LOCAL_PATH)/rootdir/init.cm.rc:root/init.cm.rc \
+	$(LOCAL_PATH)/rootdir/init.environ.rc:root/init.environ.rc \
 	$(LOCAL_PATH)/rootdir/init.microtrust.rc:root/init.microtrust.rc \
+    $(LOCAL_PATH)/rootdir/init.modem.rc:root/init.modem.rc \
     $(LOCAL_PATH)/rootdir/init.mt6735.rc:root/init.mt6735.rc \
+	$(LOCAL_PATH)/rootdir/init.mt6735.usb.rc:root/init.mt6735.usb.rc \
 	$(LOCAL_PATH)/rootdir/init.project.rc:root/init.project.rc \
-    $(LOCAL_PATH)/rootdir/init.mt6735.usb.rc:root/init.mt6735.usb.rc \
+	$(LOCAL_PATH)/rootdir/init.rc:root/init.rc \
+	$(LOCAL_PATH)/rootdir/init.ssd.rc:root/init.ssd.rc \
+	$(LOCAL_PATH)/rootdir/init.target.rc:root/init.target.rc \
+	$(LOCAL_PATH)/rootdir/init.trace.rc:root/init.trace.rc \
+	$(LOCAL_PATH)/rootdir/init.usb.rc:root/init.usb.rc \
+	$(LOCAL_PATH)/rootdir/init.usb.configfs.rc:root/init.usb.configfs.rc \
+	$(LOCAL_PATH)/rootdir/init.xlog.rc:root/init.xlog.rc \
+	$(LOCAL_PATH)/rootdir/init.zygote32.rc:root/init.zygote32.rc \
+	$(LOCAL_PATH)/rootdir/init.zygote64_32.rc:root/init.zygote64_32.rc \
     $(LOCAL_PATH)/rootdir/meta_init.modem.rc:root/meta_init.modem.rc \
     $(LOCAL_PATH)/rootdir/meta_init.project.rc:root/meta_init.project.rc \
     $(LOCAL_PATH)/rootdir/meta_init.rc:root/meta_init.rc \
+	$(LOCAL_PATH)/rootdir/ueventd.rc:root/ueventd.rc \
     $(LOCAL_PATH)/rootdir/ueventd.mt6735.rc:root/ueventd.mt6735.rc \
-    $(LOCAL_PATH)/rootdir/init/mediaserver.rc:system/etc/init/mediaserver.rc
+    $(LOCAL_PATH)/rootdir/initrc/mediaserver.rc:system/etc/init/mediaserver.rc
